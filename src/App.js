10a869dfdb2158318dc5cb1f7c97a5a718c65026
Leonardo <leonardo.otero390@gmail.com>
2021-09-03 10:41:52 -0400

    Home layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+import './Reset.css';
 import './App.css';
-import Header from './Header'
+import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
     <>
     <Header/>
+    <Home/>
     </>
   );
 }
